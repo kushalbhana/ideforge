437177dff8c4ff8a71296eb28e6fc156e60e7687
kushalbhana <kushalbhana2050@gmail.com>
2024-11-28 00:23:57 +0530

    Added Event Track Section

---
+++ components/event-track.tsx
@@ -0,0 +1,35 @@
+import { EventCard } from "./ui/event-card"
+
+export default function EventsTrack(){
+    return(
+        <>
+            <div className=" flex flex-col justify-center items-center mt-16 md:mt-44">
+                <div>
+                    <h1 className="text-2xl md:text-6xl font-bold">Our Event Track</h1>
+                </div>
+
+                <div className="flex flex-wrap justify-center items-center md:gap-2 p-4">
+                    <EventCard 
+                    content={{title: "Round 1: Ideation", 
+                    date:"December 1 - 7, 2024",
+                    image:"https://pbs.twimg.com/media/GNJpA-FWQAAtCfw.jpg", 
+                    content:"Submit a detailed document outlining your idea. Focus on identifying the problem, proposing a solution, and detailing the implementation strategy."}}
+                    />
+                    <EventCard 
+                    content={{title: "Round 2: Prototype", 
+                    date:"December 8 - 14, 2024",
+                    image:"https://pbs.twimg.com/media/GNJpA-FWQAAtCfw.jpg", 
+                    content:"Develop and implement a working prototype based on your idea. Focus on the technical execution and innovation."}}
+                    />
+                    <EventCard 
+                    content={{title: "Round 3: Presentation", 
+                    date:"December 15, 2024",
+                    image:"https://pbs.twimg.com/media/GNJpA-FWQAAtCfw.jpg", 
+                    content:"Prepare and present your idea and prototype through a PowerPoint presentation. Showcase your development process, expected outcomes, and challenges faced."}}
+                    />
+                   
+                </div>
+            </div>
+        </>
+    )
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
 import HeroSection from "../components/herosection"
 import { RoundsSection } from "../components/rounds-section"
 import { RoundsSectionSmall} from "../components/rounds-section-small"
+import EventsTrack  from "../components/event-track"
 
 export default function App() {
   return (
     <div>
       <HeroSection/>
-      <div className="w-full bg-gradient-to-b from-gray-950 dark:to-black text-slate-200 flex justify-center">
-        <div className="max-w-7xl hidden md:block">
-          <RoundsSection/>
+      <div className="w-full bg-gradient-to-b from-gray-950 dark:to-black text-slate-200 flex flex-col">
+        <div className="w-full bg-gradient-to-b from-gray-950 dark:to-black text-slate-200 flex justify-center">
+
+          <div className="max-w-7xl hidden md:block">
+            <RoundsSection/>
+          </div>
+
+          <div className="max-w-7xl md:hidden">
+            <RoundsSectionSmall/>
+          </div>
+
         </div>
-        <div className="max-w-7xl md:hidden">
-          <RoundsSectionSmall/>
+
+        <div>
+          <EventsTrack/>
         </div>
+
       </div>
+
     </div>
   )
 }
